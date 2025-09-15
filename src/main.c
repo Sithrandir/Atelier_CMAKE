44edd617f362bc062980329a1c5b35e8017247be
Sithrandir <etienne.martin1200@gmail.com>
2025-09-15 14:28:42 +0200

    feat

--- src/main.c
+++ src/main.c
@@ -4,11 +4,15 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
+	if (argc <=4)
+	{
+		char* op = argv[1];
+		char* a = argv[2];
+	}
 	if (argc == 4)
 	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
+		char* b = argv[3];
+	}
 	double r = 0;
 	if (strcmp(op, "add") == 0) {
 		r = _add(atof(a), atof(b));
@@ -26,7 +30,9 @@ int main(int argc, char *argv[]){
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else {printf("Erreur de parametres");}
+	else if (strcmp(op, "car") == 0) {
+		r = _div(atof(a), atof(b));
+		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
 	return 0;
